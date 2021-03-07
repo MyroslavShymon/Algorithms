de87d7e5647a3e8ce460ebd072f52f51a6612f06
MyroslavShymon <myroslavshymon@gmail.com>
2021-03-08 01:11:46 +0200

    sixth commit

--- src/views/BabelSort.vue
+++ src/views/BabelSort.vue
@@ -20,6 +20,8 @@
         <md-select v-model="sort" name="sort" id="sort">
           <md-option value="Bubble">Bubble sort</md-option>
           <md-option value="Shake">Shake sort</md-option>
+          <md-option value="Selection">Selection sort</md-option>
+          <md-option value="Insertion">Insertion sort</md-option>
         </md-select>
         <span class="md-error">The Sort is required</span>
       </md-field>
@@ -41,11 +43,20 @@
       <p>Сообщение задом наперёд: «{{ reversedMessage }}»</p>
     </div> -->
     <table>
-      <tr v-for="(person, index) of objects" :person="person" :key="index">
-        <td>{{ index + 1 }}</td>
-        <td>{{ person.surname }}</td>
-        <td>{{ person.group }}</td>
-      </tr>
+      <thead>
+        <tr>
+          <td>№</td>
+          <td>Surname</td>
+          <td>Group</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(person, index) of objects" :person="person" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ person.surname }}</td>
+          <td>{{ person.group }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -64,6 +75,7 @@ class Person {
     this.group = group;
   }
 }
+// let arr = [];
 let arr = [...JSON.parse(localStorage.getItem("objects"))];
 export default {
   data() {
@@ -85,7 +97,12 @@ export default {
         case "Shake":
           result = "Shake sort";
           break;
-
+        case "Selection":
+          result = "Selection sort";
+          break;
+        case "Insertion":
+          result = "Insertion sort";
+          break;
         default:
           result = "Sort";
           break;
@@ -106,7 +123,12 @@ export default {
         case "Shake":
           this.shakeSort();
           break;
-
+        case "Selection":
+          this.selectionSort();
+          break;
+        case "Insertion":
+          this.insertionSort();
+          break;
         default:
           break;
       }
@@ -116,7 +138,6 @@ export default {
       //console.log("arr", arr);
       if (this.boolean === false) {
         let swap, temp;
-
         do {
           swap = false;
           for (let index = 0; index < arr.length - 1; index++) {
@@ -130,7 +151,6 @@ export default {
         } while (swap == true);
       } else {
         let swap, temp;
-
         do {
           swap = false;
           for (let index = 0; index < arr.length - 1; index++) {
@@ -184,6 +204,79 @@ export default {
           left++;
         } while (left < right);
       }
+      this.objects = arr;
+      this.surname = "";
+      this.group = "";
+    },
+    selectionSort() {
+      arr.push(new Person(this.surname, this.group));
+      let n = arr.length;
+      if (this.boolean === false) {
+        for (let i = 0; i < n; i++) {
+          // Finding the smallest number in the subarray
+          let min = i;
+          for (let j = i + 1; j < n; j++) {
+            if (arr[j].surname < arr[min].surname) {
+              min = j;
+            }
+          }
+          if (min != i) {
+            // Swapping the elements
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+          }
+        }
+      } else {
+        for (let i = 0; i < n; i++) {
+          // Finding the smallest number in the subarray
+          let min = i;
+          for (let j = i + 1; j < n; j++) {
+            if (arr[j].group < arr[min].group) {
+              min = j;
+            }
+          }
+          if (min != i) {
+            // Swapping the elements
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+          }
+        }
+      }
+
+      this.objects = arr;
+      this.surname = "";
+      this.group = "";
+    },
+    insertionSort() {
+      arr.push(new Person(this.surname, this.group));
+      if (this.boolean === false) {
+        for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+          let indexMin = i;
+          for (let j = i + 1; j < l; j++) {
+            if (arr[indexMin].surname > arr[j].surname) {
+              indexMin = j;
+            }
+          }
+          if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+          }
+        }
+      } else {
+        for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+          let indexMin = i;
+          for (let j = i + 1; j < l; j++) {
+            if (arr[indexMin].group > arr[j].group) {
+              indexMin = j;
+            }
+          }
+          if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+          }
+        }
+      }
+
       this.objects = arr;
       this.surname = "";
       this.group = "";
